37fabed0c22e1f2e8a7347a0c3b67e36ffaaf2da
sameer34972022-code <sameer34972022@gmail.com>
2025-11-20 18:59:01 +0530

    Add files via upload

---
+++ nagin1.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("addition is :",add)
+print("subtraction is: ",sub)
+print("multiplication is: ",mul)
+print("division is : ",div)
